c14006a9d4edba946a04373bd919df8e51580613
mikhalenkadaniil <mikhalenka.d@gmail.com>
2025-12-08 15:49:20 +0100

    feat: resolve mergeObjects

--- src/objects-tasks.js
+++ src/objects-tasks.js
@@ -33,8 +33,19 @@ function shallowCopy(obj) {
  *    mergeObjects([{a: 1, b: 2}, {b: 3, c: 5}]) => {a: 1, b: 5, c: 5}
  *    mergeObjects([]) => {}
  */
-function mergeObjects(/* objects */) {
-  throw new Error('Not implemented');
+function mergeObjects(objects) {
+  const result = {};
+  objects.forEach((object) => {
+    Object.entries(object).forEach((element) => {
+      const [key, value] = element;
+      if (result[key] === undefined) {
+        result[key] = value;
+      } else {
+        result[key] += value;
+      }
+    });
+  });
+  return result;
 }
 
 /**
